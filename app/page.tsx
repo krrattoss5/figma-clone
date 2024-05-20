dcb8878231e392444b23dbc5bc08a84e8cf574b2
krrattoss5 <krrattoss5@gmail.com>
2024-05-19 22:41:49 -0500

    (add/Constants-favicon-Hooks-Lib-Types-Public)

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <Room>
-      <h1 className="text-xl">Figma Clone</h1>
-    </Room>
+    <>
+      <h1 className="text-5xl text-white">Figma Clone</h1>
+    </>
   );
 }
