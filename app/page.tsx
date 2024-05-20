425e712d8a6fa80be7ef7c1b63370fd766ae965f
krrattoss5 <krrattoss5@gmail.com>
2024-05-19 22:59:53 -0500

    (add/Live.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <>
-      <h1 className="text-5xl text-white">Figma Clone</h1>
-    </>
+    <div className="w-full h-[100vh] flex justify-center items-center text-center">
+      <h1 className="text-2xl text-white">Figma Clone</h1>
+    </div>
   );
 }
