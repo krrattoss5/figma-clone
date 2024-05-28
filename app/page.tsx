90cbd5a1144523a79bc5f812620a1468f0217cf5
krrattoss5 <krrattoss5@gmail.com>
2024-05-28 00:08:09 -0500

    (refact/now using next dynamic)

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,10 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { fabric } from 'fabric'
-import Live from "@/components/Live";
-import NavBar from "@/components/NavBar";
-import LeftSidebar from "@/components/users/LeftSidebar";
-import RightSidebar from "@/components/users/RightSidebar";
-import { CustomFabricObject } from "@/types/type";
-import { useEffect, useRef } from "react";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from '@/lib/canvas';
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
 
-export default function Page() {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  const fabricRef = useRef<fabric.Canvas | null>(null)
-  const isDrawing = useRef(false)
-  const shapeRef = useRef<fabric.Object | null>(null)
-  const selectedShapeRef = useRef<string | null>('rectangle')
-
-  useEffect(() => {
-    const canvas = initializeFabric({ canvasRef, fabricRef })
-
-    canvas.on("mouse:down", (options) => {
-      handleCanvasMouseDown({
-        options,
-        canvas,
-        isDrawing,
-        shapeRef,
-        selectedShapeRef
-      })
-    })
-
-    window.addEventListener("resize", () => {
-      handleResize( {fabricRef} )
-    })
-  }, [])
-
-  return (
-    <main className="h-screen overflow-hidden">
-      <NavBar />
-
-      <section className="flex h-full flex-row">
-        <LeftSidebar />
-          <Live canvasRef={canvasRef} />
-        <RightSidebar />
-      </section>
-    </main>
-  );
-}
+export default App;
