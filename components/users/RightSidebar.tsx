361324544b5ee57e3d3a85c18d940418bb43c9d2
krrattoss5 <krrattoss5@gmail.com>
2024-05-28 21:01:29 -0500

    (add/span: Maka changes to canvas as you like )

--- components/users/RightSidebar.tsx
+++ components/users/RightSidebar.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const RightSidebar = () => {
   return (
-    <section className='flex flex-col border-t text-primary-grey-300 min-2-[220px] sticky right-0 h-full max-sm:hidden select-none overflow-y-auto pb-20'>
+    <section className='flex flex-col border-t text-primary-grey-300 min-2-[220px] sticky right-0 h-full max-sm:hidden select-none'>
       <h3 className='px-5 pt-4 text-xs uppercase'>Design</h3>
+      <span className='text-xs text-primary-grey-300 mt-3 px-5 border-b border-green-200 pb-4'>Make changes to canvas as you like</span>
     </section>
   )
 }
