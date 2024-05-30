25b91bdd8ceb9f20f9623ace328a86aa52b66f7c
krrattoss5 <krrattoss5@gmail.com>
2024-05-29 21:50:56 -0500

    (add/Text, Color, Export & Dimensions)

--- components/users/RightSidebar.tsx
+++ components/users/RightSidebar.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Dimensions from '../settings/Dimensions'
+import Text from '../settings/Text'
+import Color from '../settings/Color'
+import Export from '../settings/Export'
+import { RightSidebarProps } from '@/types/type'
+import { modifyShape } from '@/lib/shapes'
 
-const RightSidebar = () => {
+const RightSidebar = ({
+  elementAttributes,
+  setElementAttributes,
+  fabricRef,
+  isEditingRef,
+  activeObjectRef,
+  syncShapeInStorage,
+}: RightSidebarProps) => {
+
+  const colorInputRef = useRef(null)
+  const strokeInputRef = useRef(null)
+
+  const handleInputChange = (property: string, value: string) => {
+    if(!isEditingRef.current) isEditingRef.current = true
+
+    setElementAttributes((prev) => ({
+      ...prev, [property]: value
+    }))
+
+    modifyShape({
+      canvas: fabricRef.current as fabric.Canvas,
+      property,
+      value,
+      activeObjectRef,
+      syncShapeInStorage
+    })
+  }
+
+
   return (
     <section className='flex flex-col border-t text-primary-grey-300 min-2-[220px] sticky right-0 h-full max-sm:hidden select-none'>
       <h3 className='px-5 pt-4 text-xs uppercase'>Design</h3>
       <span className='text-xs text-primary-grey-300 mt-3 px-5 border-b border-green-200 pb-4'>Make changes to canvas as you like</span>
+
+      <Dimensions
+        width={elementAttributes.width}
+        height={elementAttributes.height}
+        handleInputChange={handleInputChange}
+        isEditingRef={isEditingRef}
+      />
+      <Text
+        fontFamily={elementAttributes.fontFamily}
+        fontSize={elementAttributes.fontSize}
+        fontWeight={elementAttributes.fontWeight}
+        handleInputChange={handleInputChange}
+      />
+      <Color
+        inputRef={colorInputRef}
+        attribute={elementAttributes.fill}
+        attributeType='fill'
+        placeholder='color'
+        handleInputChange={handleInputChange}
+      />
+      <Color
+        inputRef={colorInputRef}
+        attribute={elementAttributes.stroke}
+        attributeType='stroke'
+        placeholder='stroke'
+        handleInputChange={handleInputChange}
+      />
+      <Export />
     </section>
   )
 }
