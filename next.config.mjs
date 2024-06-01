db7549f16a9050cf7f8b7a64f7b1ff8b6e9d4922
krrattoss5 <krrattoss5@gmail.com>
2024-06-01 00:06:16 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,24 @@ const nextConfig = {
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
-      "bufferutil": "commonjs bufferutil",
+      bufferutil: "commonjs bufferutil",
       canvas: "commonjs canvas",
-    })
-
-    return config
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'liveblocks.io',
-        port: ''
-      }
-    ]
+        protocol: "https",
+        hostname: "liveblocks.io",
+        port: "",
+      },
+    ],
   },
-  typescript:{
-    ignoreBuildErrors: true
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
